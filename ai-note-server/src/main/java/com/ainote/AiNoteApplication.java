51d038f676a119263a4114b89e4d30c5edc96b18
pcy <2046591497@qq.com>
2026-06-10 15:02:03 +0800

    feat: init Spring Boot project with dependencies and DDL

---
+++ ai-note-server/src/main/java/com/ainote/AiNoteApplication.java
@@ -0,0 +1,11 @@
+package com.ainote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiNoteApplication.class, args);
+    }
+}
